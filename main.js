add742b101d1112fa1fecd7f2b541e4887f38bc3
Cyanne Jones <jonescyanne@yahoo.com>
2022-04-05 19:40:33 -0600

    Add functionality for getting value from radio buttons

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 var letsCookButton = document.querySelector("#lets-cook-button");
 var resultBox = document.querySelector(".result-box");
+var radios = document.querySelectorAll('input[name="answer"]')
 
-letsCookButton.addEventListener('click', showFood);
+letsCookButton.addEventListener('click', getRadioValue);
 
-function showFood() {
-  event.preventDefault();
-  resultBox.innerText = "not a cookpot!"
+
+function getRadioValue() {
+  event.preventDefault()
+  for (var i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      resultBox.innerText = `${radios[i].value}`
+      return radios[i].value;
+    }
+  }
+};
+function getRandomNumber(array) {
+  var num = Math.floor(Math.random(array.length))
+  return num;
 };
 
 var sides = [
